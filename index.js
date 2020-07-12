e6b290e6f107334146233ce47208df4ad3acbfd4
tomoizumi <68103827+tomoizumi@users.noreply.github.com>
2020-07-12 16:00:09 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+$(window).scroll(function (){
+   $('#fadeIn').each(function(){
+       var elemPos = $(this).offset().top,
+           scroll = $(window).scrollTop(),
+           windowHeight = $(window).height();
+         if (scroll > elemPos - windowHeight + (windowHeight/3)){
+             $(this).addClass('scrollin');
+           }
+       });
+});
